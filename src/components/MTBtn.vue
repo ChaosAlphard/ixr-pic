18fa9317fcad719d45060c728a6978885de864b7
ChaosAlphard <Alphard450@outlook.com>
2019-10-28 20:23:19 +0800

    🎨add common comp

---
+++ src/components/MTBtn.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="mt-btn">
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class MTBtn extends Vue {
+  @Prop({default: false}) readonly border!: boolean
+  @Prop({default: false}) readonly block!: boolean 
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/SplitDiv.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="split-bg" :style="`background-image: url(${bg})`">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class SplitDiv extends Vue {
+  @Prop({ default: '' }) readonly bg?: string
+}
+</script>
+
+<style lang="scss" scoped>
+.split-bg {
+  width: 100%;
+  height: 280px;
+  overflow: hidden;
+  background: {
+    attachment: fixed;
+    size: cover;
+    position: center center;
+    repeat: no-repeat;
+  };
+  line-height: 280px;
+  font-size: 36px;
+  color: #FFF;
+  text-shadow: 0 0 10px #000;
+
+}
+</style>
