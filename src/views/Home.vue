5c290fb32e534fb92900577abb1020ea86dedfd6
ChaosAlphard <Alphard450@outlook.com>
2019-10-28 21:38:48 +0800

    🎨homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,12 @@
 <template>
   <div>
-    <Btn :bg="'https://s1.ax1x.com/2018/12/05/Flc7UH.jpg'">00</Btn>
-    <Btn :bg="'https://s1.ax1x.com/2018/12/05/Flc7UH.jpg'">01</Btn>
-    <Btn :bg="'https://s1.ax1x.com/2018/12/05/Flc7UH.jpg'">02</Btn>
-    <Btn :bg="'https://s1.ax1x.com/2018/12/05/Flc7UH.jpg'">03</Btn>
-    <Btn :bg="'https://s1.ax1x.com/2018/12/05/Flc7UH.jpg'">04</Btn>
-    <Btn :bg="'https://s1.ax1x.com/2018/12/05/Flc7UH.jpg'">05</Btn>
   </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
-import Btn from '@/components/SplitDiv.vue'
 
-@Component({
-  components: {
-    Btn
-  }
-})
+@Component
 export default class Home extends Vue {
 
 }
