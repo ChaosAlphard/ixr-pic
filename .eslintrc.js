7f08d714c8e4ec582ae7269be15de6d005f26abc
ChaosAlphard <Alphard450@outlook.com>
2019-10-29 11:09:20 +0800

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,9 +11,12 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'space-before-function-paren': 'off',
-    'object-curly-spacing': 'off',
-    'space-infix-ops': 'off'
+    'space-before-function-paren': [1, 'never'],
+    'keyword-spacing': 'off',
+    'object-curly-spacing': [1, 'always'],
+    'block-spacing': 0,
+    'space-infix-ops': 0,
+    'no-trailing-spaces': 'warn'
   },
   parserOptions: {
     parser: '@typescript-eslint/parser'
