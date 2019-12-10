98eba61fa3a8c3a00dd83eed836cdc5fae44c6ca
iXR <iXR@mail.com>
2019-12-10 14:31:02 +0800

    add RouteHooks

--- src/views/Section.vue
+++ src/views/Section.vue
@@ -7,17 +7,31 @@
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
 
+Component.registerHooks([
+  'beforeRouteEnter',
+  'beforeRouteLeave',
+  'beforeRouteUpdate'
+])
+
 @Component
 export default class Section extends Vue {
-  private partition: string|null = this.$route.params.section
+  private partition: string|null = null
 
   private mounted() {
     console.log(this.$route.params)
-    this.partition = this.$route.params.section
   }
 
-  private updated() {
+  private beforeRouteEnter(to:any, from:any, next: (vm:any) => void) {
+    next((vm:any) => {
+      vm.partition = vm.$route.params.section
+      console.log("enter: "+vm.$route.params.section)
+    })
+  }
+
+  private beforeRouteUpdate(to:any, from:any, next: () => void) {
     this.partition = this.$route.params.section
+    console.log("update: "+this.$route.params.section)
+    next()
   }
 }
 </script>
